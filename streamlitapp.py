9d6be65d8ed02d8713165e2fe8f7c3a0fe79d329
Umang Patel <70155633+umangptl@users.noreply.github.com>
2023-10-02 23:57:36 -0700

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -10,7 +10,6 @@ from google.oauth2 import service_account
 credentials = service_account.Credentials.from_service_account_info(
         st.secrets["gcp_service_account"]
     )
-
 client = bigquery.Client(credentials=credentials)
 
 # Load the Bay Area bike share data and cache it using st.cache_data
